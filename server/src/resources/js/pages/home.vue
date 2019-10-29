467fd2c36e6f42585a5644e2372e7da87577e12f
qvtec <qvtec3@gmail.com>
2019-10-30 04:09:01 +0900

    chart.js追加

--- server/src/resources/js/pages/home.vue
+++ server/src/resources/js/pages/home.vue
@@ -6,15 +6,148 @@
     <card :title="$t('home')">
       {{ $t('you_are_logged_in') }}
     </card>
+
+    <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+      <h1 class="h2">Dashboard</h1>
+      <div class="btn-toolbar mb-2 mb-md-0">
+        <div class="btn-group mr-2">
+          <button type="button" class="btn btn-sm btn-outline-secondary">Share</button>
+          <button type="button" class="btn btn-sm btn-outline-secondary">Export</button>
+        </div>
+        <button type="button" class="btn btn-sm btn-outline-secondary dropdown-toggle">
+          <span data-feather="calendar"></span>
+          This week
+        </button>
+      </div>
+    </div>
+
+    <chart></chart>
+
+    <h2>Section title</h2>
+    <div class="table-responsive">
+      <table class="table table-striped table-sm">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+            <th>Header</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1,001</td>
+            <td>Lorem</td>
+            <td>ipsum</td>
+            <td>dolor</td>
+            <td>sit</td>
+          </tr>
+          <tr>
+            <td>1,002</td>
+            <td>amet</td>
+            <td>consectetur</td>
+            <td>adipiscing</td>
+            <td>elit</td>
+          </tr>
+          <tr>
+            <td>1,003</td>
+            <td>Integer</td>
+            <td>nec</td>
+            <td>odio</td>
+            <td>Praesent</td>
+          </tr>
+          <tr>
+            <td>1,003</td>
+            <td>libero</td>
+            <td>Sed</td>
+            <td>cursus</td>
+            <td>ante</td>
+          </tr>
+          <tr>
+            <td>1,004</td>
+            <td>dapibus</td>
+            <td>diam</td>
+            <td>Sed</td>
+            <td>nisi</td>
+          </tr>
+          <tr>
+            <td>1,005</td>
+            <td>Nulla</td>
+            <td>quis</td>
+            <td>sem</td>
+            <td>at</td>
+          </tr>
+          <tr>
+            <td>1,006</td>
+            <td>nibh</td>
+            <td>elementum</td>
+            <td>imperdiet</td>
+            <td>Duis</td>
+          </tr>
+          <tr>
+            <td>1,007</td>
+            <td>sagittis</td>
+            <td>ipsum</td>
+            <td>Praesent</td>
+            <td>mauris</td>
+          </tr>
+          <tr>
+            <td>1,008</td>
+            <td>Fusce</td>
+            <td>nec</td>
+            <td>tellus</td>
+            <td>sed</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </section>
 </template>
 
 <script>
+import Chart from './chart';
+
 export default {
   middleware: 'auth',
+  components: {
+    Chart,
+  },
 
   metaInfo () {
     return { title: this.$t('home') }
+  },
+
+  data () {
+    return {
+      datacollection: null
+    }
+  },
+
+  mounted () {
+    this.renderChart(this.chartdata, this.options)
+  },
+
+  methods: {
+    fillData () {
+      this.datacollection = {
+        labels: [this.getRandomInt(), this.getRandomInt()],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }, {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [this.getRandomInt(), this.getRandomInt()]
+          }
+        ]
+      }
+    },
+    getRandomInt () {
+      return Math.floor(Math.random() * (50 - 5 + 1)) + 5
+    }
   }
 }
 </script>
